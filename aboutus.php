0adf066c6a65ba7a658f8cc70fa8bc9e26188597
Nilesh <nileshshrivastava7737@gmail.com>
2024-08-27 17:21:53 +0530

    commit

---
+++ aboutus.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>College Club Management System</title>
+<?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+</head>
+<style>
+
+/* Large rounded green border */
+hr.blueline {
+  border: 10px solid #00004d;
+  border-radius: 5px;
+}
+
+#bj
+{
+  font-size: 22px;
+}
+</style>
+
+
+
+  <?php require 'utils/header.php'; ?>
+  <hr class="blueline">
+  <div class="col-md-12">
+  
+<h1>About Us</h1>
+
+<p>To driving students to actively involve in learning emerging technologies in the area of Machine Learning, IoT, Networks and Data Science (MIND).</p>
+<p>To build and ecosystem that encourage our students to dig into MIND through events and competitions and workshops.</p>
+</div>
+<hr class="blueline">
+</body>
+
+ <?php require 'utils/footer.php'; ?>
+
+</html>
---
+++ utils/header.php
@@ -0,0 +1,45 @@
+<title>College Club Management System</title>
+<style>
+.bgImage {
+    background-image: url(images/MS1.png);
+    background-size: cover;
+    background-position: center;
+    height: 600px;
+    margin-bottom: 29px;
+}
+</style>
+<header class="bgImage" > 
+    <nav class="navbar" >
+        <div class="container">
+        <div class="navbar-header"><!--website name/title-->
+               
+                <a class = "navbar-brand">
+                   <h2>College Club Management System</h2>
+                </a>
+        </div>
+       
+            <ul class="nav navbar-nav navbar-right"><!--navigation-->
+                    <li><a href = "views.php"><strong>Multiple Registrations</strong></a></li>
+                    <li><a href = "index.php"><strong>Home</strong></a></li>
+                    <li><a href = "register.php"><strong>Register</strong></a></li>
+                    <li><a href = "contact.php"><strong>Contact Us</strong></a></li>
+                    <li><a href = "aboutus.php"><strong>About us</strong></a></li>
+                    <li class="btnlogout"><a class = "btn btn-default navbar-btn" href = "login_form.php">Login <span class = "glyphicon glyphicon-log-in"></span></a></li>
+
+            
+                
+                
+            </ul>
+        </div><!--container div-->
+    </nav>
+    <div class = "col-md-12">
+        <div class = "container">
+            <div class = "jumbotron"><!--jumbotron-->
+                <h1><strong>Welcome to<br></strong> MIND Spark Official Page</h1><!--jumbotron heading-->
+                <br><div class="browse d-md-flex col-md-14" >
+                <div class="row">
+                  
+            </div>
+        </div>
+    </div>
+</header>
